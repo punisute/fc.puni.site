5b0e3c0888028f6c50d1c40c992691a72954bbf8
Loutres <me@loutres.me>
2024-05-05 21:45:51 +0900

    test

--- javascript/main.js
+++ javascript/main.js
@@ -2,6 +2,6 @@ const cb = new ClipboardJS('.btn');
 
 const id = window.location.hash.substring(1);
 if (/^[a-z0-9]{8}$/i.test(id)) {
-    document.getElementById("friend-code").value = window.location.hash.substring(1);
-    document.getElementById("code-display").innerText = window.location.hash.substring(1);
+    document.getElementById("friend-code").value = id;
+    document.getElementById("code-display").innerText = id;
 }
